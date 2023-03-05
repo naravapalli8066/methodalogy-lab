87fd463e09b249399c08ae57ed3308895c3932f7
naravapalli8066 <126996279+naravapalli8066@users.noreply.github.com>
2023-03-05 18:33:09 -0500

    Add files via upload

---
+++ Traingle Test/Test cases/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traingle_Test;
+
+
+namespace Testclass
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+
+        public class TriangleTest1
+        {
+            [Test]
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int first_angle = 60;
+                int second_angle = 60;
+                int third_angle = 60;
+                string expected = "the traingle is valid";
+
+                string actual = Triangle.ValidTriangle(first_angle, second_angle, third_angle);
+
+                //Assert
+                Assert.AreEqual(expected, actual);
+
+
